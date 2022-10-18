451b431361d214c66ee7b042f89ae018b86dfdda
Toni Rantala <toni.rantala@live.fi>
2022-10-18 12:43:23 +0300

    add ons.. work in progress
    added features such as API etc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '3ce083e31db3c75c7bf8603f4839e64b', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
